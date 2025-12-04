227a70655e3e1b718edd4bcfac7b97ac0422882f
govindinnegautam <gautamgovindinne@gmail.com>
2025-12-04 09:32:45 +0530

    Data Ingestion is added

--- src/mlProject/pipeline/stage_01_data_ingestion.py
+++ src/mlProject/pipeline/stage_01_data_ingestion.py
@@ -16,3 +16,14 @@ class DataIngestionTrainingPipeline:
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file(zip_file_path=data_ingestion_config.local_data_file)
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
